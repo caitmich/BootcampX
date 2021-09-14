8cc917b31c912b22e9c4f4dafc2efc886cb1497e
Caitlin Henry <caitlinhenry.ss@gmail.com>
2021-09-14 16:09:33 -0400

    finish all queries and edit some finished queries

--- 4_queries/6_longest_average_assistance.sql
+++ 4_queries/6_longest_average_assistance.sql
@@ -1,7 +1,7 @@
-SELECT AVG(completed_at - started_at) as average_assistance_time FROM assistance_requests 
-WHERE AVG(completed_at - started_at) > ALL (
-SELECT cohorts.name, AVG(completed_at - started_at)
+SELECT cohorts.name, AVG(completed_at - started_at) as average_assistance_time
 FROM students
 JOIN assistance_requests ON students.id = student_id
 JOIN cohorts ON cohort_id = cohorts.id
-GROUP BY cohorts.name)
+GROUP BY cohorts.name
+ORDER BY average_assistance_time DESC
+LIMIT 1;
--- 4_queries/9_total_average_assistance_per_cohort.sql
+++ 4_queries/9_total_average_assistance_per_cohort.sql
@@ -1,4 +1,8 @@
-SELECT AVG(SELECT SUM(completed_at - started_at) FROM assistance_requests) as average_total_duration
+SELECT AVG(total_duration) as average_total_duration
+FROM (SELECT SUM(completed_at - started_at) as total_duration, cohorts.name
 FROM cohorts
 JOIN students ON cohorts.id = cohort_id
 JOIN assistance_requests ON students.id = student_id
+GROUP BY cohorts.name
+ORDER BY total_duration
+) as total_durations;
