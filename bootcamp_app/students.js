390fc6cd3f8aed50cfefa9dae405038a9644337f
Caitlin Henry <caitlinhenry.ss@gmail.com>
2021-09-16 11:14:08 -0400

    change teachers and students files to use parameterized queries

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -8,13 +8,21 @@ const pool = new Pool ({
   database: 'bootcampx'
 });
 
-pool.query(`
+const queryString = `
 SELECT students.id as id, students.name as name, cohorts.name as cohort_title
 FROM students
 JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE '%${args[0]}%'
-LIMIT ${args[1] || 5};
-`)
+WHERE cohorts.name LIKE $1
+LIMIT $2;
+`;
+
+const cohortName = args[0];
+const limit = args[1] || 5;
+
+const values = [`%${cohortName}%`, limit];
+
+
+pool.query(queryString, values)
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.name} has an id of ${user.id} and was in the ${user.cohort_title}`)
